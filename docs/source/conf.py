1dcb3a4c67f4d9b14ea5d4aea71f2a8ffd3811ce
Dan Hollis <27447239+sandwichi@users.noreply.github.com>
2024-09-25 21:00:04 -0400

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,8 @@
 import os
 import sys
 
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'monday-client'
@@ -34,6 +36,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 autodoc_member_order = 'bysource'
